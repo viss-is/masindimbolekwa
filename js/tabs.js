ea4242c172e4454e9f14589cc17389a98f254d38
viss-is <masindinafisa@gmail.com>
2026-01-01 17:57:28 +0200

    Create tabs.js

---
+++ js/tabs.js
@@ -0,0 +1,12 @@
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(t => t.classList.remove("active"));
+    contents.forEach(c => c.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
