a394409a839c0286277aee99cfd00dabbfe0d65c
viss-is <masindinafisa@gmail.com>
2026-01-25 18:11:57 +0200

    Delete js directory

--- js/projects.js
+++
@@ -1,8 +0,0 @@
-const projects = {
-  "hyper-vulgaris": {
-    title: "Hyper Vulgaris",
-    meta: "Oil on canvas, 40 × 80 cm, 2026",
-    description: "Meditation on land, soil, and ontological homelessness.",
-    images: ["hyper-vulgaris.jpg"]
-  }
-};
